57b03ba5940feda0bffa10c6c1ddbd6043bd07d7
Jaroslav Tóth <xtothj@gmail.com>
2022-08-03 14:19:20 +0200

    Added service for loading PCAP network interface
    - the first available interface
    - interface with specified identifier
    
    Signed-off-by: Jaroslav Tóth <xtothj@gmail.com>

--- src/main/kotlin/sk/jt/igmp/fabric/Main.kt
+++ src/main/kotlin/sk/jt/igmp/fabric/Main.kt
@@ -21,11 +21,35 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package sk.jt.igmp.fabric
+
+import pcap.spi.PacketBuffer
+import pcap.spi.PacketHeader
+import pcap.spi.Service
+import pcap.spi.exception.ErrorException
+import pcap.spi.exception.error.BreakException
+import pcap.spi.option.DefaultLiveOptions
+import sk.jt.igmp.fabric.netint.NetworkInterfaceLoaderImpl
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val service = Service.Creator.create("PcapService")
+    val interfaceLoader = NetworkInterfaceLoaderImpl(service)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val pcap = service.live(interfaceLoader.findFirstInterface(), DefaultLiveOptions())
+    try {
+        pcap.loop(
+            -1,
+            { args: String, header: PacketHeader, buffer: PacketBuffer ->
+                println("Args     : $args")
+                println("Header   : $header")
+                println("Packet   : $buffer")
+            },
+            "Hello pcap!"
+        )
+    } catch (e: BreakException) {
+        System.err.println(e.message)
+    } catch (e: ErrorException) {
+        System.err.println(e.message)
+    }
+    pcap.close()
 }
